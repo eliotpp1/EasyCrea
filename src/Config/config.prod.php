f67de7275a1867811e9e28c173efd938b1481a0c
eliotpp1 <eliot.pouplier@gmail.com>
2024-10-16 16:20:13 +0200

    v1 prod

--- src/Config/config.prod.php
+++ src/Config/config.prod.php
@@ -6,17 +6,17 @@
 /**
  * le DSN de la base
  */
-define('APP_DB_DSN', 'mysql:host=????.alwaysdata.net;dbname=?????;charset=UTF8');
+define('APP_DB_DSN', 'mysql:host=mysql-epouplier.alwaysdata.net;dbname=epouplier_easycrea;charset=UTF8');
 
 /**
  * le nom de l'utilisateur MYSQL
  */
-define('APP_DB_USER', '?????');
+define('APP_DB_USER', 'epouplier');
 
 /**
  * le mot de passe de l'utilisateur MYSQL
  */
-define('APP_DB_PASSWORD', '???????');
+define('APP_DB_PASSWORD', '\e$Zrc6)),&V)ZVBwb2{');
 
 /**
  * le préfixe des tables dans la base (utile pour les bases partagées)
